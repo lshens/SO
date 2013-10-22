e9b688826cc07703fcf5f652f4a3806b0d45522e
lucas.shen <lshens@hotmail.com>
2013-10-22 16:45:07 -0200

    Atualizada e finalizada! Signed-off-by: lucas.shen <lshens@hotmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 SO
 ==
 
-Multiplicação de matrizes com multi threads
+Multiplicação de Matrizes com Multithreads
+
+Baseada no exercicio Multiplicação de Matriz do livro - Sistemas Operacionais com Java (Abraham Silberchatz)
--- src/multiplicacao/MatrizSilberschatzs.java
+++ src/multiplicacao/MatrizSilberschatzs.java
@@ -13,49 +13,61 @@ public class MatrizSilberschatzs implements Runnable {
 		this.coluna = coluna;
 		this.A = A;
 		this.B = B;
-		this.C = C;		
+		this.C = C;
 	}
 	
 	@Override
-	public void run() {		
-		int colunasA = A[0].length;
-		
-		if (linha != coluna) {
-			throw new IllegalArgumentException("Matrizes não correspondem : "+ coluna + " diferente de " + linha);
+	public void run() {
+		int K = A[0].length;
+		for (int k = 0; k < K; k++) {
+			C [linha][coluna] += A[linha][k] * B[k][coluna];
+			System.out.println(Thread.currentThread().getName() + " Calculando " + A[linha][k] + " * " + B[k][coluna] + " = " +  A[linha][k] * B[k][coluna]);
 		}
-		
-		for (int i = 0; i < linha; i++) { // Percorre a linha
-			for (int j = 0; j < coluna; j++) { // Percorre a coluna
-				for (int k = 0; k < colunasA; k++) {
-					C [i][j] += A[i][k] * B[k][j];
-				}
-			 }
-		 }
-		
+		System.out.println(Thread.currentThread().getName() + " Resultado " + C [linha][coluna]);
 		matrizPrint(C);
 	}
 	
-	public static void matrizPrint(int[][] pMatriz) {  
-	      int linhas = pMatriz.length;  
-	      int colunas = pMatriz[0].length;  
-	      System.out.println("Resultado da Matriz = [" + linhas + "][" + colunas + "] = (");  
-	      for (int i = 0; i < linhas; i++) {  
+	public static void matrizPrint(int[][] a) {  
+	      int lines = a.length;  
+	      int cols = a[0].length;  
+	      System.out.println("array[" + lines + "][" + cols + "] = ");  
+	      for (int i = 0; i < lines; i++) {  
 	         System.out.print("|");  
-	         for (int j = 0; j < colunas; j++) {  
-	            System.out.print(" " + pMatriz[i][j] + ",");  
+	         for (int j = 0; j < cols; j++) {  
+	            System.out.print(" " + a[i][j] + ",");  
 	         }  
 	         System.out.println("|,");  
 	      }  
-	      System.out.println(");");  
-	   } 
+	      System.out.println(";");  
+	}  
 	
 	public static void main(String[] args) {
 		
-		 int aMatrix[][] = {{1, 4}, {2, 5}, {3, 6}};
-		 int bMatrix[][] = {{7, 8, 9}, {10, 11, 12}};
-		 int product[][] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-
-		 (new Thread(new MatrizSilberschatzs(aMatrix.length, bMatrix[0].length, aMatrix, bMatrix, product))).start();	
-//		 System.out.println(Thread.currentThread());
+		 int aMatriz[][] = {{1, 4}, {2, 5}, {3, 6}};
+		 int bMatriz[][] = {{7, 8, 9}, {10, 11, 12}};
+		 int produto[][] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+		 int M = aMatriz.length;
+		 int N = bMatriz[0].length;
+		 
+		 if (M != N) {
+			 throw new IllegalArgumentException("Matrizes Diferentes: " + M + "!=" + N);
+		 }
+		 
+		 int NUM_THREADS = M * N;
+		 Thread[] workers = new Thread[NUM_THREADS];
+		 int AUX_THREADS = 0;
+		 
+		 for (int i = 0; i < M; i++) { // Percorre a linha
+				for (int j = 0; j < N; j++) { // Percorre a coluna							
+							workers[AUX_THREADS] = new Thread (new MatrizSilberschatzs(i, j, aMatriz, bMatriz, produto));
+							workers[AUX_THREADS].start();
+							try {
+								workers[AUX_THREADS].join();
+							} catch (InterruptedException e) {								
+								e.printStackTrace();
+							}
+							AUX_THREADS++;		
+				}
+		 }		 
 	}
 }
